62e89f143c02ad5019e0d911f319e18424e3b3c6
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-17 19:49:40 +0200

    paginate-0

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with(["type", "technologies"])->paginate(3);
+        $projects = Project::with(["type", "technologies"])->paginate(9);
 
         return response()->json($projects);
     }
