d260b1e0da7480ed10e0c3c07b5af93424972bf1
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-18 18:13:02 +0200

    add-404-inshow-for-error

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use function Laravel\Prompts\error;
 
 class ProjectController extends Controller
 {
@@ -15,7 +16,12 @@ class ProjectController extends Controller
     }
 
     public function show($slug){
+
         $project = Project::where("slug", $slug)->with("type", "technologies")->firstOrFail();
+        
+        if(!$project){
+            error(404);
+        }
 
         return response()->json($project);
     }
